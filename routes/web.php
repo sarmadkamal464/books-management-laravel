badf179612b218bb2a3cb797fbb25e28d3aace11
sarmad <sarmadkamal464@gmail.com>
2024-03-21 21:14:15 +0500

    preview basic structure

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 
 Auth::routes();
-
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function (){
     Route::get('dashboard', function () {
         return view('home');
     })->name('dashboard');
+   
     Route::get('users', function (){
         return view('admin.users.index');
     });
