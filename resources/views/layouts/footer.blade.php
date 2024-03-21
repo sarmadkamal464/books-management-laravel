14be13a1a6f37dedcb2ec10bd30f2e97b2fbb3bc
sarmadkamal-guaranteed <sarmadkamal464@gmail.com>
2024-03-21 12:45:52 +0500

    add sweet alert 2

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,27 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 <script>
     function toggleSidebar() {
         document.querySelector('.w-64').classList.toggle('hidden');
     }
+    window.addEventListener('successMessage', event => {
+        console.log(event);
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: event.detail[0].message,
+            showConfirmButton: false,
+            timer: 1500
+        });
+    });
+    window.addEventListener('errorMessage', event => {
+        Swal.fire({
+            position: "top-end",
+            icon: "error",
+            title: event.detail[0].message,
+            showConfirmButton: false,
+            timer: 1500
+        });
+    });
 </script>
