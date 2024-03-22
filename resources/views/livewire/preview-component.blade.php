18943f7675cddc1bd6e7ec062f6f7edaa0b3b264
sarmadkamal-guaranteed <sarmadkamal464@gmail.com>
2024-03-22 14:25:37 +0500

    Update

--- resources/views/livewire/preview-component.blade.php
+++ resources/views/livewire/preview-component.blade.php
@@ -1,4 +1,11 @@
 <div class="">
+    <form class="mb-4 w-full md:mb-0 md:w-1/4">
+
+        <label class="hidden" for="search-form">Search</label>
+        <input wire:keyup="search" wire:model="searchValue" class="bg-grey-lightest border-2 focus:border-orange p-2 rounded-lg shadow-inner w-full" placeholder="Search" type="text">
+        <button class="hidden">Submit</button>
+    </form>
+    @if($filterValue == 'all' || $filterValue == 'fav')
     <div class="w-100 flex justify-end">
         <form class="max-w-sm">
             <!-- <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label> -->
@@ -9,6 +16,7 @@
         </form>
 
     </div>
+    @endif
     @if($filterValue == 'all')
     <section class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
         @foreach($books as $book)
@@ -16,11 +24,37 @@
             <a href="#">
                 <img src="{{ $book['image'] }}" alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
                 <div class="px-4 py-3 w-72">
-                    <p class="text-lg font-bold text-black truncate block capitalize">{{ $book->name }}</p>
+                    <p class="text-lg font-bold text-black truncate block capitalize">{{ $book->title }}</p>
+                    <p class="text-sm font-thin">{{ implode(' ', array_slice(str_word_count($book->description, 1), 0, 25)) }}</p>
                     <div class="flex items-center">
-                        <p class="text-lg font-semibold text-black cursor-auto my-3">{{ $book->price }}</p>
+                        <p class="text-lg font-semibold text-black cursor-auto my-3">${{ $book->price }}</p>
                         <div class="ml-auto">
                             <svg wire:click="addFav({{$book->id}})" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 heart-svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="flex justify-end">
+                        <button wire:click="readMore({{$book->id}})" type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Read More</button>
+                    </div>
+                </div>
+            </a>
+        </div>
+        @endforeach
+    </section>
+    @elseif($filterValue == 'fav')
+    <h1>Fav</h1>
+    <section class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+        @foreach($favBooks as $favBook)
+        <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="#">
+                <img src="{{ $favBook->book->image }}" alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+                <div class="px-4 py-3 w-72">
+                    <p class="text-lg font-bold text-black truncate block capitalize">{{ $favBook->book->title }}</p>
+                    <div class="flex items-center">
+                        <p class="text-lg font-semibold text-black cursor-auto my-3">${{ $favBook->book->price }}</p>
+                        <div class="ml-auto">
+                            <svg wire:click="removeFav({{$favBook->id}})" xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 heart-svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                             </svg>
                         </div>
@@ -30,23 +64,47 @@
         </div>
         @endforeach
     </section>
-    @elseif($filterValue == 'fav')
-    <h1>Fav</h1>
+    @elseif($filterValue == 'preview')
 
+    <div class="p-10">
+        <!--Card 1-->
+        <div class=" w-full lg:max-w-full lg:flex">
+            <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url({{$bookDetail->image}})" title="Mountain">
+            </div>
+            <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                <div class="mb-8">
+                    <div class="text-gray-900 font-bold text-xl mb-2">{{$bookDetail->title}}</div>
+                    <p class="text-gray-700 text-base">{{$bookDetail->description}}</p>
+                </div>
+                <div class="flex items-center">
+                    <img class="w-10 h-10 rounded-full mr-4" src="{{$bookDetail->image}}" alt="Avatar of Writer">
+                    <div class="text-sm">
+                        <p class="text-gray-900 leading-none">John Smith</p>
+                        <p class="text-gray-600">Aug 18</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @elseif($filterValue == 'search')
     <section class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-        @foreach($favBooks as $favBook)
+        @foreach($searchBooks as $book)
         <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
             <a href="#">
-                <img src="{{ $favBook->book->image }}" alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+                <img src="{{ $book['image'] }}" alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
                 <div class="px-4 py-3 w-72">
-                    <p class="text-lg font-bold text-black truncate block capitalize">{{ $favBook->book->name }}</p>
+                    <p class="text-lg font-bold text-black truncate block capitalize">{{ $book->title }}</p>
+                    <p class="text-sm font-thin">{{ implode(' ', array_slice(str_word_count($book->description, 1), 0, 25)) }}</p>
                     <div class="flex items-center">
-                        <p class="text-lg font-semibold text-black cursor-auto my-3">{{ $favBook->book->price }}</p>
+                        <p class="text-lg font-semibold text-black cursor-auto my-3">${{ $book->price }}</p>
                         <div class="ml-auto">
-                            <svg wire:click="removeFav({{$favBook->id}})" xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 heart-svg">
+                            <svg wire:click="addFav({{$book->id}})" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 heart-svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                             </svg>
                         </div>
+                    </div>
+                    <div class="flex justify-end">
+                        <button wire:click="readMore({{$book->id}})" type="button" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Read More</button>
                     </div>
                 </div>
             </a>
