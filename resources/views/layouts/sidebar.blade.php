6706ea1e20eaf8c905c9a2c53a27a54de12a4987
sarmadkamal-guaranteed <sarmadkamal464@gmail.com>
2024-03-22 10:53:41 +0500

    Add Middleware For User Management

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,33 +2,34 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
 
 <div class="w-64 flex  hidden sm:block">
-	<ul class="text-gray-700">
+    <ul class="text-gray-700">
 
-		<li class="block cursor-pointer p-2 hover:bg-gray-800 hover:text-gray-300">
-		  <a class="" href="{{route('admin.dashboard')}}">
-			<span class="w-8 mb-5 relative">
-			  <i class="w-8 fas fa-file-alt p-2 rounded-full mx-2"></i>
-			  <span class="absolute right-0 top-0 -mt-2 text-xs bg-yellow-500 text-black font-medium px-2 rounded-full">3</span>
-			</span>
-			Dashboard
-		  </a>
-		</li>
-		<li class="block cursor-pointer p-2 hover:bg-gray-800 hover:text-gray-300">
-		  <a class="" href="{{ route('admin.books') }}">
-			<i class="w-8 fas fa-paper-plane p-2 rounded-full mx-2"></i>
-			Books
-		  </a>
-		</li>
-		<li class="block cursor-pointer p-2 hover:bg-gray-800 hover:text-gray-300">
-		  <a class="" href="#">
-			<i class="w-8 fas fa-search p-2 rounded-full mx-2"></i>
-			Favorites
-		  </a>
-		</li>
-	  </ul>
-		</div>
-
-
-
-
-
+        <li class="block cursor-pointer p-2 hover:bg-gray-800 hover:text-gray-300">
+            <a class="" href="{{route('admin.dashboard')}}">
+                <span class="w-8 mb-5 relative">
+                    <i class="w-8 fas fa-file-alt p-2 rounded-full mx-2"></i>
+                    <span class="absolute right-0 top-0 -mt-2 text-xs bg-yellow-500 text-black font-medium px-2 rounded-full">3</span>
+                </span>
+                Dashboard
+            </a>
+        </li>
+        <li class="block cursor-pointer p-2 hover:bg-gray-800 hover:text-gray-300">
+            <a class="" href="{{ route('admin.books') }}">
+                <i class="w-8 fas fa-paper-plane p-2 rounded-full mx-2"></i>
+                Books
+            </a>
+        </li>
+        <li class="block cursor-pointer p-2 hover:bg-gray-800 hover:text-gray-300">
+            <a class="" href="{{ route('admin.users') }}">
+                <i class="w-8 fas fa-paper-plane p-2 rounded-full mx-2"></i>
+                Users
+            </a>
+        </li>
+        <li class="block cursor-pointer p-2 hover:bg-gray-800 hover:text-gray-300">
+            <a class="" href="#">
+                <i class="w-8 fas fa-search p-2 rounded-full mx-2"></i>
+                Favorites
+            </a>
+        </li>
+    </ul>
+</div>
